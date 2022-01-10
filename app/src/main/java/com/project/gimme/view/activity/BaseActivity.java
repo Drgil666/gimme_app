73e0724591c8450f3f7d091d29aa5930212a8e3e
Drgil666 <2574105038@qq.com>
2022-01-10 16:07:20 +0800

    Feats:修复了无法强制竖屏的bug，新增MessageFragment。

--- app/src/main/java/com/project/gimme/view/activity/BaseActivity.java
+++ app/src/main/java/com/project/gimme/view/activity/BaseActivity.java
@@ -1,6 +1,8 @@
 package com.project.gimme.view.activity;
 
 import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,6 +13,13 @@ import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
  * @date 2022/1/8 16:48
  */
 public class BaseActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        System.out.println(111);
+    }
+
     @Override
     protected void attachBaseContext(Context newBase) {
         super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
---
+++ app/src/main/java/com/project/gimme/view/fragment/MessageFragment.java
@@ -0,0 +1,22 @@
+package com.project.gimme.view.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.project.gimme.R;
+
+/**
+ * @author DrGilbert
+ */
+public class MessageFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_message, container, false);
+    }
+}
