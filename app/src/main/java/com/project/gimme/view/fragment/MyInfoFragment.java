25bde1348705c171227858f158e0cb7029afb663
Drgil666 <2574105038@qq.com>
2022-01-23 14:02:33 +0800

    Feats:格式化部分代码，修复部分功能点击时无法出现灰色的bug，新增部分图片点击时的灰色滤镜。

--- app/src/main/java/com/project/gimme/view/fragment/MyInfoFragment.java
+++ app/src/main/java/com/project/gimme/view/fragment/MyInfoFragment.java
@@ -1,10 +1,15 @@
 package com.project.gimme.view.fragment;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
+import android.widget.ImageView;
 
 import androidx.fragment.app.Fragment;
 
@@ -20,14 +25,16 @@ import java.util.List;
  */
 public class MyInfoFragment extends Fragment {
     private List<MyInfoListVO> myInfoList = new ArrayList<>();
+    private ImageView userInfoIcon;
     private GridView myInfoGridView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_my_info, container, false);
-//        System.out.println("myInfoFragment");
+        userInfoIcon = view.findViewById(R.id.user_info_icon);
         myInfoGridView = view.findViewById(R.id.gridview_my_info);
+        initUserInfoLayout();
         initMyInfoGridView();
         return view;
     }
@@ -45,6 +52,21 @@ public class MyInfoFragment extends Fragment {
             myInfoListVO.setType(i);
             myInfoList.add(myInfoListVO);
         }
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private void initUserInfoLayout() {
+        userInfoIcon.setImageResource(R.mipmap.app_icon);
+        userInfoIcon.setOnClickListener(view -> System.out.println("click!"));
+        userInfoIcon.setOnTouchListener((view, motionEvent) -> {
+            if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                ((ImageView) view).clearColorFilter(); // 清除滤镜效果
+            } else if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                ((ImageView) view).setColorFilter(Color.GRAY, PorterDuff.Mode.MULTIPLY); // 设置滤镜效果
+                //如果return true的话,onClick的事件就不会触发!
+            }
+            return false;
+        });
     }
 
     private void initMyInfoGridView() {
