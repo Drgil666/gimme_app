c21ef70dc06361b24da091c0817349c2d4472429
Drgil666 <2574105038@qq.com>
2022-01-28 16:38:46 +0800

    Feats:新增paramActivity,优化部分UI交互体验。

--- app/src/main/java/com/project/gimme/utils/BundleUtil.java
+++ app/src/main/java/com/project/gimme/utils/BundleUtil.java
@@ -9,4 +9,6 @@ public class BundleUtil {
     public static final String CHAT_TYPE_ATTRIBUTE = "chat_type";
     public static final String PERSONAL_MSG_TYPE_ATTRIBUTE = "personal_msg_type";
     public static final String CHAT_FILE_INFO_ID_ATTRIBUTE = "chat_file_info_id";
+    public static final String PARAM_NAME_ATTRIBUTE = "param_name";
+    public static final String PARAM_VALUE_ATTRIBUTE = "param_value";
 }
