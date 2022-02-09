004c9895bb2d1da41b181ef6e721039058226205
Drgil666 <2574105038@qq.com>
2022-02-09 13:43:56 +0800

    Feats:更新UserUtil部分字段。

--- app/src/main/java/com/project/gimme/utils/UserUtil.java
+++ app/src/main/java/com/project/gimme/utils/UserUtil.java
@@ -11,40 +11,15 @@ import lombok.Getter;
  */
 public class UserUtil {
     public static final String ROOT_ATTRIBUTE = "root";
+    public static final String GROUP_USER_ATTRIBUTE = "group_user";
     public static final String GROUP_OWNER_ATTRIBUTE = "group_owner";
     public static final String GROUP_ADMIN_ATTRIBUTE = "group_admin";
     public static final String CHANNEL_OWNER_ATTRIBUTE = "channel_owner";
+    public static final String CHANNEL_USER_ATTRIBUTE = "channel_user";
     public static final String USER_ATTRIBUTE = "user";
     public static final String MALE_ATTRIBUTE = "男";
     public static final String FEMALE_ATTRIBUTE = "女";
     public static final String OTHER_GENDER_ATTRIBUTE = "其他";
-
-    @AllArgsConstructor
-    @Getter
-    public enum Character {
-        /**
-         * 超级用户
-         */
-        TYPE_ROOT(0, ROOT_ATTRIBUTE),
-        /**
-         * 群聊群主
-         */
-        TYPE_GROUP_OWNER(1, GROUP_OWNER_ATTRIBUTE),
-        /**
-         * 群聊管理员
-         */
-        TYPE_GROUP_ADMIN(2, GROUP_ADMIN_ATTRIBUTE),
-        /**
-         * 频道广播用户
-         */
-        TYPE_CHANNEL_OWNER(3, CHANNEL_OWNER_ATTRIBUTE),
-        /**
-         * 普通用户
-         */
-        TYPE_USER(4, USER_ATTRIBUTE);
-        private final Integer code;
-        private final String name;
-    }
 
     @AllArgsConstructor
     @Getter
@@ -65,18 +40,71 @@ public class UserUtil {
         private final String name;
     }
 
-    public static final Character[] CHARACTER_LIST = Character.values();
-    public static final HashMap<String, Integer> CHARACTER_MAP = getCharacterMap();
+    @AllArgsConstructor
+    @Getter
+    public enum UserCharacter {
+        /**
+         * 超级用户
+         */
+        TYPE_ROOT(0, ROOT_ATTRIBUTE),
+        /**
+         * 普通用户
+         */
+        TYPE_USER(1, USER_ATTRIBUTE);
+        private final Integer code;
+        private final String name;
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public enum GroupCharacter {
+        /**
+         * 超级用户
+         */
+        TYPE_ROOT(0, ROOT_ATTRIBUTE),
+        /**
+         * 群聊群主
+         */
+        TYPE_GROUP_OWNER(1, GROUP_OWNER_ATTRIBUTE),
+        /**
+         * 群聊管理员
+         */
+        TYPE_GROUP_ADMIN(2, GROUP_ADMIN_ATTRIBUTE),
+        /**
+         * 群聊普通成员
+         */
+        TYPE_GROUP_USER(3, GROUP_USER_ATTRIBUTE);
+        private final Integer code;
+        private final String name;
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public enum ChannelCharacter {
+        /**
+         * 超级用户
+         */
+        TYPE_ROOT(0, ROOT_ATTRIBUTE),
+        /**
+         * 频道广播用户
+         */
+        TYPE_CHANNEL_OWNER(4, CHANNEL_OWNER_ATTRIBUTE),
+        /**
+         * 频道普通用户
+         */
+        TYPE_CHANNEL_USER(5, CHANNEL_USER_ATTRIBUTE);
+        private final Integer code;
+        private final String name;
+    }
+
+    public static final UserCharacter[] USER_CHARACTER_LIST = UserCharacter.values();
+    public static final HashMap<String, Integer> USER_CHARACTER_MAP = getUserCharacterMap();
+    public static final GroupCharacter[] GROUP_CHARACTER_LIST = GroupCharacter.values();
+    public static final HashMap<String, Integer> GROUP_CHARACTER_MAP = getGroupCharacterMap();
+    public static final ChannelCharacter[] CHANNEL_CHARACTER_LIST = ChannelCharacter.values();
+    public static final HashMap<String, Integer> CHANNEL_CHARACTER_MAP = getChannelCharacterMap();
     public static final Gender[] GENDER_LIST = Gender.values();
     public static final HashMap<String, Integer> GENDER_MAP = getGenderMap();
-
-    public static HashMap<String, Integer> getCharacterMap() {
-        HashMap<String, Integer> hashMap = new HashMap<>(10);
-        for (Character character : CHARACTER_LIST) {
-            hashMap.put(character.name, character.code);
-        }
-        return hashMap;
-    }
 
     public static HashMap<String, Integer> getGenderMap() {
         HashMap<String, Integer> hashMap = new HashMap<>(10);
@@ -86,13 +114,6 @@ public class UserUtil {
         return hashMap;
     }
 
-    public static Integer getCharacterByName(String name) {
-        if (CHARACTER_MAP.containsKey(name)) {
-            return CHARACTER_MAP.get(name);
-        }
-        return null;
-    }
-
     public static Integer getGenderByName(String name) {
         if (GENDER_MAP.containsKey(name)) {
             return GENDER_MAP.get(name);
@@ -100,4 +121,48 @@ public class UserUtil {
         return null;
     }
 
+    public static HashMap<String, Integer> getUserCharacterMap() {
+        HashMap<String, Integer> hashMap = new HashMap<>(10);
+        for (UserCharacter character : USER_CHARACTER_LIST) {
+            hashMap.put(character.name, character.code);
+        }
+        return hashMap;
+    }
+
+    public static Integer getUserCharacterByName(String name) {
+        if (USER_CHARACTER_MAP.containsKey(name)) {
+            return USER_CHARACTER_MAP.get(name);
+        }
+        return null;
+    }
+
+    public static HashMap<String, Integer> getGroupCharacterMap() {
+        HashMap<String, Integer> hashMap = new HashMap<>(10);
+        for (GroupCharacter character : GROUP_CHARACTER_LIST) {
+            hashMap.put(character.name, character.code);
+        }
+        return hashMap;
+    }
+
+    public static Integer getGroupCharacterByName(String name) {
+        if (GROUP_CHARACTER_MAP.containsKey(name)) {
+            return GROUP_CHARACTER_MAP.get(name);
+        }
+        return null;
+    }
+
+    public static HashMap<String, Integer> getChannelCharacterMap() {
+        HashMap<String, Integer> hashMap = new HashMap<>(10);
+        for (ChannelCharacter character : CHANNEL_CHARACTER_LIST) {
+            hashMap.put(character.name, character.code);
+        }
+        return hashMap;
+    }
+
+    public static Integer getChannelCharacterByName(String name) {
+        if (CHANNEL_CHARACTER_MAP.containsKey(name)) {
+            return CHANNEL_CHARACTER_MAP.get(name);
+        }
+        return null;
+    }
 }
