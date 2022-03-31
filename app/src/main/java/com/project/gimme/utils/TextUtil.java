e6b1df7b3435709c51453f2ddfd7a189337647f5
Drgil666 <2574105038@qq.com>
2022-03-31 17:12:52 +0800

    Feats:优化文字输出的排版。

--- app/src/main/java/com/project/gimme/utils/TextUtil.java
+++ app/src/main/java/com/project/gimme/utils/TextUtil.java
@@ -5,18 +5,25 @@ package com.project.gimme.utils;
  * @date 2022/3/27 14:38
  */
 public class TextUtil {
+    private static String regex = "[\ud83c\udc00-\ud83c\udfff]|[\ud83d\udc00-\ud83d\udfff]|[\u2600-\u27ff]";
+
     public static String expandableText(String text) {
         int cnt = 0;
         StringBuilder newText = new StringBuilder();
         for (int i = 0; i < text.length(); i++) {
-            if ((int) text.charAt(i) > 255) {
+            if (i + 1 < text.length() && (text.charAt(i) + String.valueOf(text.charAt(i + 1))).matches(regex)) {
                 cnt += 2;
+                newText.append(text.charAt(i) + String.valueOf(text.charAt(i + 1)));
+                i++;
+            } else if ((int) text.charAt(i) > 255) {
+                cnt += 2;
+                newText.append(text.charAt(i));
             } else {
                 cnt++;
+                newText.append(text.charAt(i));
             }
-            newText.append(text.charAt(i));
             if (cnt >= 10 && i != text.length() - 1) {
-                cnt -= 10;
+                cnt = 0;
                 newText.append("\n");
             }
         }
